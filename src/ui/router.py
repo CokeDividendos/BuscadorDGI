1a3058a92cf7b156938aadf46ccaf1f76fd0fec6
CokeDividendos <coke.g.castro@gmail.com>
2026-02-27 14:35:18 -0300

    Fix formatting of radio button in navigation menu

--- src/ui/router.py
+++ src/ui/router.py
@@ -112,7 +112,8 @@ def run_app():
             current_idx = 0
 
         # Menú navegación principal como lista vertical (st.radio)
-        page_section = st.radio("### Navegación"
+        page_section = st.radio(
+            "Navegación",
             page_sections,
             index=current_idx,
             key="page_section_radio"
